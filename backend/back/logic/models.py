e27944e1cab5262c1b2c5c86be3116834e94e6cf
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-07-25 13:11:51 +0300

    REVIEWS model

--- backend/back/logic/models.py
+++ backend/back/logic/models.py
@@ -21,4 +21,10 @@ class Review(models.Model):
     shamba = models.ForeignKey(Shamba, on_delete=models.SET_NULL,null=True)
     user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200,null=True,blank=True)
+    rating = models.IntegerField(null=True,blank=True,default=0)
+    comment = models.TextField(null=True,blank=True)
+    id = models.AutoField(primary_key=True,editable=False)
+
+    def __str__(self):
+        return self.rating    
           

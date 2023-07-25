221c318832436dfb28a4f69bbfb5c8c727673445
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-07-25 14:07:32 +0300

    main urls

--- backend/back/logic/models.py
+++ backend/back/logic/models.py
@@ -42,7 +42,7 @@ class OrderItem(models.Model):
     shamba = models.ForeignKey(Shamba,on_delete=models.SET_NULL,null=True)
     order = models.ForeignKey(Order,on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200,blank=True,null=True)
-    price = models.DecimalField(decimal_places=2,max_digits=7,null=True,blank=True)
+    price = models.DecimalField(decimal_places=2,max_digits=7, null=True, blank=True)
     image = models.ImageField(null=True,blank=True)
     _id = models.AutoField(primary_key=True,editable=False)
 

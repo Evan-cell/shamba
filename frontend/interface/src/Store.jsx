5db83214264e0e4be8cb4aca0ab3b35537174b72
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-07-27 00:59:15 +0300

    store

--- frontend/interface/src/Store.jsx
+++ frontend/interface/src/Store.jsx
@@ -2,3 +2,14 @@ import {combineReducers,applyMiddleware} from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
+import {shambaListReducer} from './reducers/ShambaReducer'
+
+const reducer = combineReducers({
+    shambaList:shambaListReducer,
+})
+
+const middlware = [thunk]
+
+const store = configureStore({reducer}, composeWithDevTools(applyMiddleware(...middlware)))
+
+export default store
